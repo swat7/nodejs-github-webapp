0dd1d327469d811f4866e6c7a9926719d3be3cdf
swat7 <swathi.nagaraj7@gmail.com>
2015-09-15 23:06:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-<<<<<<< HEAD
-=======
-// views is directory for all template files
->>>>>>> 4f1011bf53d09bf1f0388cee4da09aaface4ebad
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
